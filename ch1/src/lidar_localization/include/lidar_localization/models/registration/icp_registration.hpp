bc39b0f73111cf13268f23b3bbe05fc1b3ac3046
yandld <1453363089@qq.com>
2020-11-01 12:11:58 +0800

    add ch1

---
+++ ch1/src/lidar_localization/include/lidar_localization/models/registration/icp_registration.hpp
@@ -0,0 +1,32 @@
+/*
+ * @Description: icp 匹配模块
+ * @Author: Ren Qian
+ * @Date: 2020-02-08 21:46:57
+ */
+#ifndef LIDAR_LOCALIZATION_MODELS_REGISTRATION_ICP_REGISTRATION_HPP_
+#define LIDAR_LOCALIZATION_MODELS_REGISTRATION_ICP_REGISTRATION_HPP_
+
+#include <pcl/registration/icp.h>
+#include "lidar_localization/models/registration/registration_interface.hpp"
+
+namespace lidar_localization {
+class ICPRegistration: public RegistrationInterface {
+  public:
+    ICPRegistration(const YAML::Node& node);
+    ICPRegistration(float res, float step_size, float trans_eps, int max_iter);
+
+    bool SetInputTarget(const CloudData::CLOUD_PTR& input_target) override;
+    bool ScanMatch(const CloudData::CLOUD_PTR& input_source, 
+                   const Eigen::Matrix4f& predict_pose, 
+                   CloudData::CLOUD_PTR& result_cloud_ptr,
+                   Eigen::Matrix4f& result_pose) override;
+  
+  private:
+    bool SetRegistrationParam(float res, float step_size, float trans_eps, int max_iter);
+
+  private:
+    pcl::IterativeClosestPoint<CloudData::POINT, CloudData::POINT>::Ptr ndt_ptr_;
+};
+}
+
+#endif
